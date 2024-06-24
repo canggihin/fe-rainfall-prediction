9f8510e59b3ec4c20c2df606b0fc942f9c21d70f
mchwalfajar <mchwalfajar@gmail.com>
2024-06-24 18:13:01 +0700

    update path

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -3,10 +3,11 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 
 import DashboardLayout from 'src/layouts/dashboard';
 
+export const DevicePage = lazy(() => import('src/pages/device'));
 export const IndexPage = lazy(() => import('src/pages/app'));
 export const UserPage = lazy(() => import('src/pages/user'));
-export const LoginPage = lazy(() => import('src/pages/login'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
+export const LoginPage = lazy(() => import('src/pages/login'))
 
 // ----------------------------------------------------------------------
 
@@ -22,16 +23,17 @@ export default function Router() {
       ),
       children: [
         { element: <IndexPage />, index: true },
-        { path: 'user', element: <UserPage /> },
+        { path: 'data', element: <UserPage /> },
+        { path: 'device', element: <DevicePage /> },
       ],
-    },
-    {
-      path: 'login',
-      element: <LoginPage />,
     },
     {
       path: '404',
       element: <Page404 />,
+    },
+    {
+      path: 'login',
+      element: <LoginPage />,
     },
     {
       path: '*',
